b9f31d7349dacddae952d1a570674156c0e7157f
Tanish Shah <tanibshah@gmail.com>
2020-08-31 12:06:38 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailers.rb
@@ -0,0 +1,11 @@
+class ContactMailers < ApplicationMailers::Base
+    default to: 'tanibshah@gmail.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end
